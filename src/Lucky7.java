d46dd1d7581ccc83d27234591b74b8bc9d84fa12
Kairit Kuivalahti <kairit.ojamae@gmail.com>
2025-10-21 21:35:07 +0300

    Commit 2 – Rahat, voitot ja toisto
    Lisätään rahat, voittohäviölogiikka ja looppi, mutta ei vielä aloitusrahan kysymistä eikä kyllä/ei-jatkokysymystä.

--- src/Lucky7.java
+++ src/Lucky7.java
@@ -2,10 +2,39 @@ public class Lucky7 {
     public static void main(String[] args) {
         System.out.println("Tervetuloa Lucky7-peliin!");
 
-        int n1 = (int)(Math.random() * 10) + 1;
-        int n2 = (int)(Math.random() * 10) + 1;
-        int n3 = (int)(Math.random() * 10) + 1;
+        int money = 5;
 
-        System.out.println("\nArvotut numerot: " + n1 + ", " + n2 + ", " + n3);
+        while (money > 0) {
+            money--;
+
+            int n1 = (int)(Math.random() * 10) + 1;
+            int n2 = (int)(Math.random() * 10) + 1;
+            int n3 = (int)(Math.random() * 10) + 1;
+
+            System.out.println("\nArvotut numerot: " + n1 + ", " + n2 + ", " + n3);
+
+            int sevens = 0;
+            if (n1 == 7) sevens++;
+            if (n2 == 7) sevens++;
+            if (n3 == 7) sevens++;
+
+            if (sevens == 3) {
+                System.out.println("Kaikki kolme seiskaa! Saat +10 rahaa!");
+                money += 10;
+            } else if (sevens == 2) {
+                System.out.println("Kaksi seiskaa! Saat +5 rahaa!");
+                money += 5;
+            } else if (sevens == 1) {
+                System.out.println("Yksi seiska! Saat +3 rahaa!");
+                money += 3;
+            } else {
+                System.out.println("Ei yhtään seiskaa. Hävisit tämän kierroksen.");
+            }
+
+            System.out.println("Rahaa jäljellä: " + money);
+        }
+
+        System.out.println("Rahat loppuivat. Peli päättyy.");
     }
 }
+
